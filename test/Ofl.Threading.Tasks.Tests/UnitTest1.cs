93d81932bb292dbc303dbf2f34dcb6033c92b269
casperOne <casperOne@caspershouse.com>
2017-05-18 16:29:39 -0400

    Initial code push.

---
+++ test/Ofl.Threading.Tasks.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ofl.Threading.Tasks.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
