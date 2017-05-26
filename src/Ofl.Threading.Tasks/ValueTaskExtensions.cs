9cf09bf1a891e5a6dec303e01bbc9aaba22020d0
casperOne <casperOne@caspershouse.com>
2017-05-26 13:24:34 -0400

    Added RunInContext to ValueTaskExtensions.

--- src/Ofl.Threading.Tasks/ValueTaskExtensions.cs
+++ src/Ofl.Threading.Tasks/ValueTaskExtensions.cs
@@ -1,9 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Ofl.Threading.Tasks
 {
     public static class ValueTaskExtensions
     {
         public static ValueTask<T> FromResult<T>(T result) => new ValueTask<T>(result);
+
+        public static T RunInContext<T>(this ValueTask<T> task) => task.AsTask().RunInContext();
     }
 }
